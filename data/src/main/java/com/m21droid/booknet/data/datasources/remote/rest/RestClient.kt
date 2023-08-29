9e24b208f7f601bd6286425c131e421ace32c4c9
m21droid <mmushkalik@gmail.com>
2023-08-29 04:34:52 +0300

    Updated RestClient

--- data/src/main/java/com/m21droid/booknet/data/datasources/remote/rest/RestClient.kt
+++ data/src/main/java/com/m21droid/booknet/data/datasources/remote/rest/RestClient.kt
@@ -1,6 +1,7 @@
 package com.m21droid.booknet.data.datasources.remote.rest
 
 import android.content.Context
+import android.os.Build
 import com.m21droid.booknet.data.datasources.remote.rest.Const.HTTP_CACHE
 import com.m21droid.booknet.data.datasources.remote.rest.Const.MAX_SIZE
 import com.m21droid.booknet.data.datasources.remote.rest.Const.TIMEOUT
@@ -9,12 +10,46 @@ import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import java.io.File
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit.SECONDS
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.X509TrustManager
 
 class RestClient(private val context: Context) {
 
     val okHttpClient: OkHttpClient =
-        OkHttpClient.Builder().apply {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            OkHttpClient.Builder()
+        } else {
+            try {
+                val trustManager = object : X509TrustManager {
+                    override fun checkClientTrusted(
+                        chain: Array<X509Certificate>, authType: String,
+                    ) {
+                    }
+
+                    override fun checkServerTrusted(
+                        chain: Array<X509Certificate>, authType: String,
+                    ) {
+                    }
+
+                    override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+                }
+                val sslContext = SSLContext.getInstance("SSL")
+                sslContext.init(null, arrayOf(trustManager), SecureRandom())
+                val sslSocketFactory: SSLSocketFactory = sslContext.socketFactory
+                OkHttpClient.Builder().apply {
+                    sslSocketFactory(sslSocketFactory, trustManager)
+                    hostnameVerifier { _, _ ->
+                        true
+                    }
+                }
+            } catch (e: Exception) {
+                OkHttpClient.Builder()
+            }
+        }.apply {
             val file = File(context.cacheDir, HTTP_CACHE)
             val cache = Cache(file, MAX_SIZE.toLong())
             cache(cache)
